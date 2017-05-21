0ced9350c4013bfd9ceb3ff6fdbdb27ba1c49212
Beni Warmers <benjamin.warmers@voicepoint.ch>
2017-05-21 23:14:11 +0200

    OIDC implemented

--- src/SimplCommerce.WebHost/Startup.cs
+++ src/SimplCommerce.WebHost/Startup.cs
@@ -17,6 +17,7 @@ using SimplCommerce.Module.Core.Models;
 using SimplCommerce.Module.Localization;
 using SimplCommerce.WebHost.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace SimplCommerce.WebHost
 {
@@ -34,6 +35,12 @@ namespace SimplCommerce.WebHost
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", true, true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
 
             builder.AddEnvironmentVariables();
             var connectionStringConfig = builder.Build();
@@ -107,6 +114,13 @@ namespace SimplCommerce.WebHost
             app.UseCustomizedRequestLocalization();
             app.UseCustomizedStaticFiles(env);
             app.UseCustomizedIdentity();
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                ClientId = Configuration["OpenIdConnect:ClientId"],
+                ClientSecret = Configuration["ClientSecret"],
+                Authority = Configuration["OpenIdConnect:Authority"],
+                ResponseType = OpenIdConnectResponseType.Code
+            });
             app.UseCustomizedMvc();
         }
     }
